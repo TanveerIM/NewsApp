ee665c876bd618083894625057e7aff71fa11188
tanveer <tanveeratworks@gmail.com>
2024-11-08 09:56:46 +0530

    creating appEntry useCase and getnews repository class. and model class for news api call

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -1,29 +1,18 @@
 package com.example.newsapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.lifecycleScope
-import com.example.newsapp.domain.usecases.AppEntryUseCase
 import com.example.newsapp.presentation.navgraph.NavGraph
-import com.example.newsapp.presentation.onboarding.OnBoardingScreen
-import com.example.newsapp.presentation.onboarding.OnBoardingViewModel
 import com.example.newsapp.ui.theme.NewsAppTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/example/newsapp/data/remote/NewsApi.kt
+++ app/src/main/java/com/example/newsapp/data/remote/NewsApi.kt
@@ -1,6 +1,7 @@
 package com.example.newsapp.data.remote
 
 import com.example.newsapp.data.remote.dto.NewsResponse
+import com.example.newsapp.util.Constants.API_KEY
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -10,6 +11,6 @@ interface NewsApi {
     suspend fun getNews(
         @Query("page") page: Int,
         @Query("sources") sources: String,
-        @Query("apiKey") apiKey: String
+        @Query("apiKey") apiKey: String = API_KEY
     ) : NewsResponse
 }
